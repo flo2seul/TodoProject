63dfa5118f8b7d46fbafd032bf6ec47cce7e987e
seulayoon <seulaayoon@gmail.com>
2021-11-12 12:39:08 +0900

    create: service architecture fin

--- demo/src/main/java/com/example/demo/controller/TodoController.java
+++ demo/src/main/java/com/example/demo/controller/TodoController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.TodoDTO;
-import com.example.demo.model.TodoEntity;
+import com.example.demo.dto.ResponseDTO;
+
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +15,20 @@ import java.util.List;
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+
+    @Autowired
+    private TodoService service;
+
+
     //todo 메서드 작성하기
-    @GetMapping("/TodoEntity")
-    public ResponseEntity testTodo() {
-//        List<String> list = new ArrayList<>();
-//        list.add("todo test");
-        TodoDTO response = new TodoDTO("test","test",true);
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo() {
+        String str = service.testSerivice(); //테스트 서비스 사용
+        List<String> list = new ArrayList<>();
+        list.add(str);
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
         return ResponseEntity.ok().body(response);
+
 
     }
 }
