50f7e01979608a8d35ad7ed8ee652d6a9c24dacf
seulayoon <seulaayoon@gmail.com>
2021-11-14 11:46:49 +0900

    create: AddTodo.js & UI design start

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import Todo from './Todo';
+import { Paper, List} from "@material-ui/core";
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+   
+    this.state = {
+      items: [
+        {id: "0", title: "Hello World 1", done: true },
+        {id: "0", title: "Hello World 2", done: false },
+      ],
+    };
+  }
   render() {
-    return (
-      <div className="App">
-        <Todo />
-        <Todo />
-      </div>
-    );
+    var todoItems = this.state.items.length> 0 && (
+      <Paper style={{ margin:16 }}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+    ); 
+    return <div className="App">{todoItems}</div>;
   }
 }
 
