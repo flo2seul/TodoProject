0172143602d20647c0116ce453ffaedc3a2170cd
seulayoon <seulaayoon@gmail.com>
2021-11-16 11:37:40 +0900

    user 로그인 기능 구현 1

--- demo/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ demo/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    private final long MAX_AGE_SECS  = 3600;
+    private final long MAX_AGE_SECS = 3600;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -14,7 +14,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 //Origin이 http:localhost:3000에 대해
                 .allowedOrigins("http://localhost:3000")
                 //GET,POST,PUT,PATCH,DELETE,OPTIONS 메서드를 허용한다.
-                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(MAX_AGE_SECS);
